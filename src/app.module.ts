e3a38d510700495d569c38f36e7c8bb613bc5c99
Ilya Medvedev <medvedeff-ii@yandex.ru>
2020-05-25 00:33:44 +0300

    next

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CropModule } from './crop/crop.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ProductModule],
+  imports: [TypeOrmModule.forRoot(), ProductModule, CropModule],
   controllers: [AppController],
   providers: [AppService],
 })
