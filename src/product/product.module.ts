e4de1361811ea382e49170806f74705f2e2ab8ad
Ilya Medvedev <medvedeff-ii@yandex.ru>
2020-05-24 16:10:48 +0300

    added crud mapping

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
-import { ProductService } from './product.service';
+import { ProductsService } from './product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductsService]
 })
 export class ProductModule {}
